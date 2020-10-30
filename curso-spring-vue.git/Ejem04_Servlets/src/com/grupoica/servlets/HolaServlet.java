21620d54d15089ac3d6662679534510fc5d5cff9
luisborrallo <73582925+luisborrallo@users.noreply.github.com>
2020-10-30 11:41:33 +0100

    Servlets

--- curso-spring-vue.git/Ejem04_Servlets/src/com/grupoica/servlets/HolaServlet.java
+++ curso-spring-vue.git/Ejem04_Servlets/src/com/grupoica/servlets/HolaServlet.java
@@ -7,27 +7,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HolaServlet
- */
-@WebServlet("/HolaServlet")
+
+@WebServlet("/hola")
 public class HolaServlet extends HttpServlet {
+
+	
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public HolaServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+     super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("<h1>Hola desde Served </h1>at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String strNombre=req.getParameter("nombre");
+		System.out.println("Parametro:" + strNombre);
+		String html="<html><head><title>Formulario envio</title></html><body>";
+		
+		if(strNombre == null || "".equals(strNombre)) {
+			html += "<h2>Pon el nombre</h2>";
+		}
+		else {
+			html += "<form action='./hola.do' method='post'>" + 
+			"Veces: <input type='number' name='veces' />" + 
+			"<input type='submit' value='POST' /></form>";
+		}
+		html +="</body></html>";
+		
+		resp.getWriter().append(html);
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		/**
+		 * 1 vez
+		 * 2 vez
+		 * 3 vez
+		 * .....
+		 */
+		String strVeces=req.getParameter("veces");
+		int iVeces = Integer.parseInt(strVeces);
+		
+		String htmlVeces = "";
+		for (int i=0; i<=iVeces; i++) {
+			htmlVeces += "<p>  vez " + i +"</p>";
+		}
+		
+		resp.getWriter().append(htmlVeces);
 	}
 
 }
