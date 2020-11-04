9211502102381f19d33d50bf556d0595d09370b3
luisborrallo <73582925+luisborrallo@users.noreply.github.com>
2020-11-04 19:15:09 +0100

    vue js

--- curso-spring-vue.git/Vue/Ej01_IntroVue.js
+++ curso-spring-vue.git/Vue/Ej01_IntroVue.js
@@ -1,10 +1,18 @@
 Vue.component("app-saludo", {
     "template": `<h2>Hola soy un componente</h2> `
+
 });
 Vue.component("app-adios", {
     "template": `
     <h4>Hasta luego</h4>
      `
+});
+Vue.component("app-despedida", {
+    "template": `
+    <div><h2> A la voluntad de Dios </h2>
+    <h2>Pues hasta luego!!!!!!!</h2>
+    </div>`
+
 });
 new Vue({
     "el": "#app-section",
